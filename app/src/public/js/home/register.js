a2c1b7fe8a87e117370ee956e0ff34213dfd8678
QKRtjdqja <p.harry0228@gmail.com>
2024-07-30 18:58:52 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,15 +1,19 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+  name = document.querySelector("#name"),
   psword = document.querySelector("#psword"),
-  loginBtn = document.querySelector("#button");
+  confirmPsword = document.querySelector("#confirm-psword"),
+  registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login(){
+function register(){
   const req = {
     id: id.value,
+    name: name.value,
     psword: psword.value,
+    confirmPsword: confirmPsword.value,
   };
 
   fetch("/register", {
@@ -22,13 +26,13 @@ function login(){
   .then((res) => res.json())
   .then((res) => {
     if(res.success){
-      location.href = "/login";
+      location.href = "/";
     } else{
       alert(res.msg);
     } 
   })
   .catch((err) => {
-    console.error("회원가입 중 에러 발생");
+    console.error("로그인 중 에러 발생");
   });
 };
 
