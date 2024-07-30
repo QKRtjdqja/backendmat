773d8e914c945461ba99280a2c60aadde625a160
QKRtjdqja <p.harry0228@gmail.com>
2024-07-30 20:09:43 +0900

    user 모델 추가

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,7 +12,7 @@ function login(){
     psword: psword.value,
   };
 
-  fetch("/register", {
+  fetch("/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -22,13 +22,13 @@ function login(){
   .then((res) => res.json())
   .then((res) => {
     if(res.success){
-      location.href = "/login";
+      location.href = "/";
     } else{
       alert(res.msg);
     } 
   })
   .catch((err) => {
-    console.error("회원가입 중 에러 발생");
+    console.error("로그인 중 에러 발생");
   });
 };
 
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -26,13 +26,13 @@ function register(){
   .then((res) => res.json())
   .then((res) => {
     if(res.success){
-      location.href = "/";
+      location.href = "/login";
     } else{
       alert(res.msg);
     } 
   })
   .catch((err) => {
-    console.error("로그인 중 에러 발생");
+    console.error("회원가입 중 에러 발생");
   });
 };
 
